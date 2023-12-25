7ccfc64e9209d5971d08b9e0bb1fff68673c72ed
Admin <gorodilovmax0@gmail.com>
2023-12-26 00:00:50 +0400

    Добавьте файлы проекта.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = { -5, 33, 55, 6, 7, 22, 22, -60, 38, 20, 17, -5 };
+            int temp;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+
+                }
+            }
+
+
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
